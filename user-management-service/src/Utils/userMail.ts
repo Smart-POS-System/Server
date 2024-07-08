73b62677ba36a55c65c325f0f41044b0cd2bf8b7
Visitha Wickramasinghe <wickramasinghevisitha@gmail.com>
2024-07-08 19:10:27 +0530

    Implemented sending emails in production

--- user-management-service/src/Utils/userMail.ts
+++ user-management-service/src/Utils/userMail.ts
@@ -7,21 +7,41 @@ interface MailOptions {
 }
 
 export const sendMail = async (options: MailOptions): Promise<void> => {
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST_DEV as string,
-    port: Number(process.env.EMAIL_PORT_DEV),
-    auth: {
-      user: process.env.EMAIL_USERNAME_DEV as string,
-      pass: process.env.EMAIL_PASSWORD_DEV as string,
-    },
-  });
+  if (process.env.NODE_ENV === "development") {
+    const transporter = nodemailer.createTransport({
+      host: process.env.EMAIL_HOST_DEV as string,
+      port: Number(process.env.EMAIL_PORT_DEV),
+      auth: {
+        user: process.env.EMAIL_USERNAME_DEV as string,
+        pass: process.env.EMAIL_PASSWORD_DEV as string,
+      },
+    });
 
-  const mailOptions = {
-    from: "Polos POS System <polos@uom.lk>",
-    to: options.email,
-    subject: options.subject,
-    text: options.message,
-  };
+    const mailOptions = {
+      from: "Polos POS System <polos@uom.lk>",
+      to: options.email,
+      subject: options.subject,
+      text: options.message,
+    };
 
-  await transporter.sendMail(mailOptions);
+    await transporter.sendMail(mailOptions);
+  }
+  if (process.env.NODE_ENV === "production") {
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.EMAIL_PROD,
+        pass: process.env.PASSWORD_PROD,
+      },
+    });
+
+    const mailOptions = {
+      from: process.env.EMAIL_PROD,
+      to: options.email,
+      subject: options.subject,
+      text: options.message,
+    };
+
+    await transporter.sendMail(mailOptions);
+  }
 };
