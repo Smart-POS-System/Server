e998eb0628661310ed173b10f2614af5ad940d08
Nethmal Wijesinghe <130041936+Nethmalwje@users.noreply.github.com>
2024-08-25 19:04:13 +0530

    email-srv_commit

--- email-service/src/index.js
+++ email-service/src/index.js
@@ -1,11 +1,8 @@
 import express, { json } from "express";
 import { testRoute } from "./routes/test-route.js";
-<<<<<<< Updated upstream
-=======
 import { specs, swaggerUi } from "./swagger.js";
 import { sample } from "./routes/sample.js";
 //test1
->>>>>>> Stashed changes
 
 const app = express();
 const port = 3000;
